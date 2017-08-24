8e99c292a775d3912a085a27d6225d30ecd7955f
Anwaar <developer.anwar@gmail.com>
2017-08-24 15:56:39 -0400

    First Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(	
+	<div><h1> Hello React!</h1></div>
+	, document.getElementById('root')
+
+);
+registerServiceWorker();
